af3714bfcd7dc721919b9ea34f4d2d14c0b91ade
Punit Tomar <punittomar777@gmail.com>
2025-10-25 13:08:12 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
